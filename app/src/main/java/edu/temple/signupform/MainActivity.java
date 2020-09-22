9dcf7daabdec4fc98b82b3bcf43a503d85939bcc
minyi <tug22058@temple.edu>
2020-09-21 20:45:32 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/signupform/MainActivity.java
@@ -0,0 +1,23 @@
+package edu.temple.signupform;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    AlertDialog.Builder alertDialog;
+    Button signup;
+    EditText name,email,password, passwordConfirm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.formactivity);
+    }
+
+
+}
