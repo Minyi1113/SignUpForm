187222b3a454142fe350296893ce408873b98a87
minyi <tug22058@temple.edu>
2020-09-23 03:47:40 -0400

    complete to display Toast

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -13,10 +13,8 @@ import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
-    AlertDialog.Builder alertDialog;
     Button SignUp;
     EditText name,email,password, passwordConfirm;
-    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +28,6 @@ public class FormActivity extends AppCompatActivity {
         password = (EditText)findViewById(R.id.password);
         passwordConfirm = (EditText) findViewById(R.id.passwordConfirm);
 
-        //creating alertbox for showing the username after successfull signup
-        alertDialog=new AlertDialog.Builder(FormActivity.this);
-
         //checking the empty edittext
         SignUp.setOnClickListener(new View.OnClickListener() {
 
@@ -44,13 +39,11 @@ public class FormActivity extends AppCompatActivity {
                 {
                     //if yes then toast will apear to show the fields are mandatory
                     Toast.makeText(getApplicationContext(), "All Fields Are Mandatory !", Toast.LENGTH_SHORT).show();
-
                 }
                 else if(!password.getText().toString().equals(passwordConfirm.getText().toString()))
                 {
                     //if password mismatch then this toast will apear
                     Toast.makeText(getApplicationContext(), "Password Do not Match !", Toast.LENGTH_SHORT).show();
-
                 }
                 else
                 {
